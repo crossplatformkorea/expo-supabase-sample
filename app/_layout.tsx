f3413f3fca63ff93728e351aa751b166a2b9624b
hyochan <dooboolab@gmail.com>
2023-02-11 22:20:10 +0900

    fix: lint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import {useCallback, useEffect, useState} from 'react';
 
 import Icons from '../src/utils/Icons';
 import RootProvider from '../src/providers';
-import { Slot } from 'expo-router';
+import {Slot} from 'expo-router';
 import {View} from 'react-native';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import {useAssets} from 'expo-asset';
